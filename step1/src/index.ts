e980af1d0610c14d0414e87b6cb6aa54c223cc54
Marcin Golenia <marcin@WPLT001928.bayer.cnb>
2022-01-25 11:47:13 +0100

    First step - bare nodejs ts api

---
+++ step1/src/index.ts
@@ -0,0 +1,13 @@
+console.log('Starting the app')
+
+import express from 'express'
+import helloRouter from './router'
+
+const app = express()
+const port = 3000
+
+app.use(helloRouter)
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`)
+});
