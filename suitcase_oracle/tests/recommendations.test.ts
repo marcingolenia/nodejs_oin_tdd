b19b77363f4d8dd45fd7e6152f43d168fd852de8
Marcin 👾 Golenia <marcingolenia@gmail.com>
2022-10-03 13:40:33 +0200

    Failing test for jacket below 11C

--- suitcase_oracle/tests/recommendations.test.ts
+++ suitcase_oracle/tests/recommendations.test.ts
@@ -9,3 +9,13 @@ it(`I want to receive 1 tshirt & 1 pair of socks and 1 panties per day recommend
     // Assert
     expect(response.body).toStrictEqual(expectedClothes)
 })
+
+it(`I want to receive 1 jacket recommendation if any day in the date range has temperature below 11 C°`, async () => {
+    // Arrange
+    // TODO : below 11 C - I need to control the temperature to make the test reliable?
+    const expectedClothes = ['2 tshirt', '2 pair of socks', '2 panties', '1 jacket']
+    // Act
+    const response = await request(app).get('/recommendation?from=2022-10-03&to=2022-10-05')
+    // Assert
+    expect(response.body).toStrictEqual(expectedClothes)
+})
